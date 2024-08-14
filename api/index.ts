7cac25454cdb77514187f04f3017cfa8559daca4
Rakshith Sindhiya R <rakshithsindhiyar@Rakshiths-MacBook-Pro.local>
2024-08-14 08:26:34 +0530

    added fe

--- api/index.ts
+++ api/index.ts
@@ -1,5 +1,7 @@
 import { createSchema, createYoga } from "graphql-yoga";
 import express from "express";
+import { fileURLToPath } from "url";
+import path from "path";
 
 const app = express();
 
@@ -17,6 +19,10 @@ const resolvers = {
   },
 };
 
+// Since we are using E6 modules for NodeJS, __dirname will not be present for modules, we need to do some work around as below for making __dirname to work
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 const yoga = createYoga({
   schema: createSchema({
     typeDefs,
@@ -25,6 +31,13 @@ const yoga = createYoga({
 });
 
 app.use("/graphql", yoga);
+
+app.use(express.static(path.resolve(__dirname, "..", "frontend", "build")));
+app.get("*", (req, res) => {
+  res.sendFile(
+    path.resolve(__dirname, "..", "frontend", "build", "index.html")
+  );
+});
 
 app.listen(SERVER_PORT, () => {
   console.log(`Server is running on port: ${SERVER_PORT}`);

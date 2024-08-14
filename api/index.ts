6f62a7aa18a22185d333d4aafec4d9bf3546ed14
Rakshith Sindhiya R <rakshithsindhiyar@Rakshiths-MacBook-Pro.local>
2024-08-14 08:36:32 +0530

    connected fe with be

--- api/index.ts
+++ api/index.ts
@@ -19,10 +19,6 @@ const resolvers = {
   },
 };
 
-// Since we are using E6 modules for NodeJS, __dirname will not be present for modules, we need to do some work around as below for making __dirname to work
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-
 const yoga = createYoga({
   schema: createSchema({
     typeDefs,
@@ -32,11 +28,12 @@ const yoga = createYoga({
 
 app.use("/graphql", yoga);
 
-app.use(express.static(path.resolve(__dirname, "..", "frontend", "build")));
+// Serve the static files from the React app
+app.use(express.static(path.join(__dirname, "../frontend/build")));
+
+// Handle React routing, return all requests to the React app
 app.get("*", (req, res) => {
-  res.sendFile(
-    path.resolve(__dirname, "..", "frontend", "build", "index.html")
-  );
+  res.sendFile(path.join(__dirname, "../frontend/build", "index.html"));
 });
 
 app.listen(SERVER_PORT, () => {

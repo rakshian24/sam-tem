304cda2cad52490254a2a5178d91daf116bb4039
Rakshith Sindhiya R <rakshithsindhiyar@Rakshiths-MacBook-Pro.local>
2024-08-14 09:12:28 +0530

    8th time

--- api/index.ts
+++ api/index.ts
@@ -1,12 +1,11 @@
-import { createSchema, createYoga } from "graphql-yoga";
 import express from "express";
-import { fileURLToPath } from "url";
+import { createYoga, createSchema } from "graphql-yoga";
 import path from "path";
 
 const app = express();
+const PORT = process.env.PORT || 4001;
 
-const SERVER_PORT = 4001;
-
+// GraphQL setup
 const typeDefs = /* GraphQL */ `
   type Query {
     hello: String!
@@ -29,13 +28,13 @@ const yoga = createYoga({
 app.use("/graphql", yoga);
 
 // Serve the static files from the React app
-app.use(express.static(path.join(__dirname, "../frontend/build")));
+app.use(express.static(path.join(__dirname, "./build")));
 
 // Handle React routing, return all requests to the React app
 app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "../frontend/build", "index.html"));
+  res.sendFile(path.join(__dirname, "./build", "index.html"));
 });
 
-app.listen(SERVER_PORT, () => {
-  console.log(`Server is running on port: ${SERVER_PORT}`);
+app.listen(PORT, () => {
+  console.log(`Server is running on port: ${PORT}`);
 });
